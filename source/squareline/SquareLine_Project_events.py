138b0f434ef5ce1f06a08297788a713a0e2aa137
Frank Bemelman <bemelman@protonmail.com>
2026-06-30 13:47:09 +0200

    Add files via upload

---
+++ source/squareline/SquareLine_Project_events.py
@@ -0,0 +1,288 @@
+
+def opensettings(event_struct):
+    return
+
+
+def Mute(event_struct):
+    return
+
+
+def CheckDatabase(event_struct):
+    return
+
+
+def Calibration(event_struct):
+    return
+
+
+def CalibrationSetButton(event_struct):
+    return
+
+
+def CalibrationNextButton(event_struct):
+    return
+
+
+def click1(event_struct):
+    return
+
+
+def click2(event_struct):
+    return
+
+
+def click3(event_struct):
+    return
+
+
+def click4(event_struct):
+    return
+
+
+def click5(event_struct):
+    return
+
+
+def expertmode(event_struct):
+    return
+
+
+def StationScroll(event_struct):
+    return
+
+
+def RadioGlobeClick(event_struct):
+    return
+
+
+def StationScrollreleased(event_struct):
+    return
+
+
+def StationScrolllongpressed(event_struct):
+    return
+
+
+def StationScrollVal(event_struct):
+    return
+
+
+def StationRollClicked(event_struct):
+    return
+
+
+def StationScrollPressed(event_struct):
+    return
+
+
+def StationScollClicked(event_struct):
+    return
+
+
+def StationRollPressed(event_struct):
+    return
+
+
+def StationRollVal(event_struct):
+    return
+
+
+def DoCalibration(event_struct):
+    return
+
+
+def beep(event_struct):
+    return
+
+
+def bep(event_struct):
+    return
+
+
+def SetupEnter(event_struct):
+    return
+
+
+def RebuildDataBaseClick(event_struct):
+    return
+
+
+def RollDown(event_struct):
+    return
+
+
+def RollUp(event_struct):
+    return
+
+
+def Station1(event_struct):
+    return
+
+
+def Station2(event_struct):
+    return
+
+
+def Station3(event_struct):
+    return
+
+
+def Station4(event_struct):
+    return
+
+
+def Station1store(event_struct):
+    return
+
+
+def station1(event_struct):
+    return
+
+
+def station2(event_struct):
+    return
+
+
+def station3(event_struct):
+    return
+
+
+def station4(event_struct):
+    return
+
+
+def CalibrationAdvanced(event_struct):
+    return
+
+
+def CalibrationSetAsZero(event_struct):
+    return
+
+
+def CalibrationPoint(event_struct):
+    return
+
+
+def CalibratePoint(event_struct):
+    return
+
+
+def RollerWEChanged(event_struct):
+    return
+
+
+def RollerNSchanged(event_struct):
+    return
+
+
+def RollerNSChanged(event_struct):
+    return
+
+
+def RollerNSorEWChanged(event_struct):
+    return
+
+
+def CalibrationLattituded(event_struct):
+    return
+
+
+def CalibrationLattitudes(event_struct):
+    return
+
+
+def CalibrationLongitudes(event_struct):
+    return
+
+
+def CalibrationEnter(event_struct):
+    return
+
+
+def DatabaseEnter(event_struct):
+    return
+
+
+def ResetAll(event_struct):
+    return
+
+
+def ResetAllEnter(event_struct):
+    return
+
+
+def RebuildDatabase(event_struct):
+    return
+
+
+def PowerCycle(event_struct):
+    return
+
+
+def PowerCylce(event_struct):
+    return
+
+
+def StationRollMoved(event_struct):
+    return
+
+
+def PowerCycle2(event_struct):
+    return
+
+
+def SaveVolTone(event_struct):
+    return
+
+
+def CalibrationLatitudes(event_struct):
+    return
+
+
+def StationInfo(event_struct):
+    return
+
+
+def SpeakerToggle(event_struct):
+    return
+
+
+def SetHomeEnter(event_struct):
+    return
+
+
+def BlueToothToggle(event_struct):
+    return
+
+
+def PortalButton(event_struct):
+    return
+
+
+def PowerCyle(event_struct):
+    return
+
+
+def ClockFlagToggle(event_struct):
+    return
+
+
+def SwitchToClock(event_struct):
+    return
+
+
+def ClockHomePower(event_struct):
+    return
+
+
+def ClockFaceClick(event_struct):
+    return
+
+
+def MapClicked(event_struct):
+    return
+
+
+def SerialNumberButton(event_struct):
+    return
+
