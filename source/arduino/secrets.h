b9057998381aa628dd53476106300ee741fb4fcc
Frank Bemelman <bemelman@protonmail.com>
2026-07-15 21:23:15 +0200

    Delete source/arduino/secrets.h

--- source/arduino/secrets.h
+++
@@ -1,4 +0,0 @@
-char SSID [32] = "YOUR-SSID";
-char PASSWORD[32] = "YOUR-WIFI-PASSWORD";
-char google_api_key[] = "YOUR-GOOGLE-API-KEY";
-char open_weather_map_api_key[] = "YOUR-OPEN-WEATHER-API-KEY";
